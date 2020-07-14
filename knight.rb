9414c90d793fb13f359d3ed5960f1a7a85ed3b39
Mike Esposito <mikeesposito6852@gmail.com>
2020-07-14 12:00:10 -0700

    Add initial placeholder methods

--- knight.rb
+++ knight.rb
@@ -1,2 +1,12 @@
 class Knight
+  attr_reader :position, :possibilities
+
+  def initialize(position)
+    @position = position
+    @possibilities = build_tree(position)
+  end
+
+  def build_tree(position)
+    [[0,1],[0,2]]
+  end
 end
--- main.rb
+++ main.rb
@@ -1,2 +1,7 @@
 require_relative 'board.rb'
 require_relative 'knight.rb'
+
+Board.new
+knight = Knight.new([0,0])
+p knight.position
+p knight.possibilities
