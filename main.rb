4bb110c12d220baaf32a6b502e88c53cc6c925f5
Mike Esposito <mikeesposito6852@gmail.com>
2020-07-16 08:46:39 -0700

    Refactor Tree initialize method and improve variable names in build_tree method

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative 'board.rb'
 require_relative 'knight.rb'
 require_relative 'tree.rb'
 
-tree = Tree.new([0, 0])
+tree = Tree.new([3, 3])
 p tree.root
 p tree.root.nnw
 p tree.root.nne
--- tree.rb
+++ tree.rb
@@ -12,20 +12,24 @@ class Tree
                 }
 
   def initialize(position)
-    @root = build_tree(position)
+    @root = Knight.new
+    @root.position = position
+    build_tree(@root)
   end
 
   def on_board?(position)
     position.all? { |e| e > -1 && e < 8 }
   end
 
-  def build_tree(position)
-    root = Knight.new
-    root.position = position
-    DIRECTIONS.each do |k, v|
-      temp = root.position.map.with_index { |e, i| i == 0 ? e += v[0] : e += v[1] }
+  def build_tree(root)
+    return root if root.nil?
+
+    DIRECTIONS.each do |direction, shifts|
+      temp = root.position.map.with_index do |coordinate, i|
+        i == 0 ? coordinate += shifts[0] : coordinate += shifts[1]
+      end
       if on_board?(temp)
-        node = root.send("#{k}=", Knight.new)
+        node = root.send("#{direction}=", Knight.new)
         node.position = temp
       end
     end
