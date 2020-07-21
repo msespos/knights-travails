d19c669e47c81d0afa3a1890a1a1e54056320df9
Mike Esposito <mikeesposito6852@gmail.com>
2020-07-21 08:33:31 -0700

    Add user interface and user feedback to main

--- board.rb
+++ board.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # stores methods for creating and displaying the board
+# used for debugging purposes only
 class Board
   def initialize
     @board = Array.new(8) { Array.new(8) { ' - ' } }
--- main.rb
+++ main.rb
@@ -7,8 +7,22 @@ require_relative 'tree.rb'
 def knight_moves(start, destination)
   tree = Tree.new(start)
   solution = tree.knight_tree(nil, destination)
-  p solution.path_to
-  tree.print_filled_board
+  puts "You made it in #{solution.path_to.length - 1} moves! Here's your path:"
+  solution.path_to.each { |position| p position }
 end
 
-def start knight_moves([0, 0], [3, 3])
+def intro
+  puts 'Enter the x-coordinate of your starting position (0 to 7):'
+  x_coord_start = gets.chomp
+  puts 'Enter the y-coordinate of your starting position (0 to 7):'
+  y_coord_start = gets.chomp
+  puts 'Enter the x-coordinate of your destination (0 to 7):'
+  x_coord_destination = gets.chomp
+  puts 'Enter the y-coordinate of your destination (0 to 7):'
+  y_coord_destination = gets.chomp
+  start = [x_coord_start.to_i, y_coord_start.to_i]
+  destination = [x_coord_destination.to_i, y_coord_destination.to_i]
+  knight_moves(start, destination)
+end
+
+intro
